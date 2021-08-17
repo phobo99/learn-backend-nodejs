3d763da9e6beacc8a42685cb1e837ba74c820b6e
phobo99 <phobotkcb@gmail.com>
2021-08-17 22:43:52 +0700

    connect db mysql with sequelize

--- Nodejs/src/route/web.js
+++ Nodejs/src/route/web.js
@@ -1,11 +1,12 @@
 import express from "express";
+import homeController from "../controllers/HomeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("Hello world with Pho")
-    });
+    router.get('/', homeController.getHomePage);
+
+    router.get('/about', homeController.getAboutPage);
 
     return app.use("/", router);
 }
