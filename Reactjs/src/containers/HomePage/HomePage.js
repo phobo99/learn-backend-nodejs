a24746d66018aaa753144048b9bfbfbbe4063a73
phobo99 <phobotkcb@gmail.com>
2021-09-13 23:19:21 +0700

    ReactJS: Carousel basic (not complete)

--- Reactjs/src/containers/HomePage/HomePage.js
+++ Reactjs/src/containers/HomePage/HomePage.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
+import Specialty from './Section/Specialty';
 
 class HomePage extends Component {
 
     render() {
 
         return (
-            <HomeHeader />
+            <div>
+                <HomeHeader />
+                <Specialty />
+                <div style={{ height: '300px' }}></div>
+            </div>
         );
     }
 
