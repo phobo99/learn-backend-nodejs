1c1367353027dc9a7e415978f00f8267763ef0b9
phobo99 <phobotkcb@gmail.com>
2021-09-30 02:42:04 +0700

    edit API

--- Nodejs/src/services/userService.js
+++ Nodejs/src/services/userService.js
@@ -141,6 +141,7 @@ let createNewUser = (data) => {
                     gender: data.gender,
                     roleId: data.roleId,
                     positionId: data.positionId,
+                    image: data.avatar
                 })
 
                 resolve({
@@ -202,6 +203,9 @@ let updateUserData = (data) => {
                 user.positionId = data.positionId;
                 user.gender = data.gender;
                 user.phonenumber = data.phonenumber;
+                if (data.avatar) {
+                    user.image = data.avatar;
+                }
 
                 await user.save();
                 // await db.User.save({
