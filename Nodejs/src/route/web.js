e2b5fcbf1bab73c3504e0b37161e187d91cc7597
phobo99 <phobotkcb@gmail.com>
2021-10-05 23:33:47 +0700

    API get all doctors

--- Nodejs/src/route/web.js
+++ Nodejs/src/route/web.js
@@ -24,6 +24,7 @@ let initWebRoutes = (app) => {
 
     router.get('/api/allcode', userController.getAllCode)
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome)
+    router.get('/api/get-all-doctor', doctorController.getAllDoctors)
 
 
     return app.use("/", router);
--- Nodejs/src/services/doctorService.js
+++ Nodejs/src/services/doctorService.js
@@ -26,7 +26,26 @@ let getTopDoctorHome = (limitInput) => {
         }
     })
 }
+let getAllDoctors = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let doctors = await db.User.findAll({
+                where: { roleId: 'R2' },
+                attributes: {
+                    exclude: ['password', 'image']
+                },
+            })
+            resolve({
+                errCode: 0,
+                data: doctors
+            })
+        } catch (e) {
+            reject(e)
 
+        }
+    })
+}
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors
 }
